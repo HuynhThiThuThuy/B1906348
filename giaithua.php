cf168bb5f7419ec2b4e7ca7c510d9daa6583bb0a
HuynhThiThuThuy <102167522+HuynhThiThuThuy@users.noreply.github.com>
2022-03-22 22:08:05 +0700

    Add files via upload

---
+++ giaithua.php
@@ -0,0 +1,15 @@
+<?php 
+	$n = 10;
+	function tinhGiaithua($n) {
+		$giai_thua = 1;
+		if ($n == 0 || $n == 1) {
+			return $giai_thua;
+		} else {
+			for($i = 2; $i <= $n; $i ++) {
+				$giai_thua *= $i;
+			}
+			return $giai_thua;
+		}
+	}
+	echo ("Giai thừa của " . $n . " là: " . tinhGiaithua ( $n ));
+?>
---
+++ nhap.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+Pasword: <input type="password" name="pwd"><br>
+E-mail: <input type="text" name="email"><br>
+Date : <input type="date" name="date"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
